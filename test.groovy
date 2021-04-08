2242152376657961ba7e233cbb4b910c34c5cb2e
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-08 15:40:36 +0300

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,11 +1,2 @@
-node('master') {
-def jobParam = []
-def listFile = readFile("test.txt").readLines()
-listFile.each {
-        jobParam.add(choice(choices: listFile, desciption: '', name: 'animals'))
-}
-
-properties([disableConcurrentBuilds(), parameters(jobParam)])
-
 println ('Ok')
-}
+
