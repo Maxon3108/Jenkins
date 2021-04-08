042589e6d2d2e6c4e2f64fbaf1b44b7d47fe05a3
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-08 21:20:43 +0300

    Update test3.groovy

--- test3.groovy
+++ test3.groovy
@@ -1,43 +1,71 @@
-def userId = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0].userId
-fruit_fans = '["ann","bob","zoe"]'
-def code = sprintf("""
-if ( "%s" in %s) {
-    choices = ["Apple","Banana","Cherry"]
-} else {
-    choices = ["Amaranth","Beet","Cabbage"]
+import groovy.transform.Field
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript
+
+...
+
+@Field
+def repoApp = 'https://<Git repository URL of App>'
+
+@Field
+def props = []
+
+@Field
+def newParams = []
+
+...
+
+setNewProps()
+
+...
+// Setup job properties
+void setNewProps() {
+
+    //Parameters are unknown at first load
+    try {
+        regenerateJob = (params.RegenerateJob == null) ? true : params.RegenerateJob
+    }
+    catch (MissingPropertyException e) {
+        regenerateJob = true
+    }
+
+    if (regenerateJob) {
+        def dynBranchFB = new SecureGroovyScript("""
+def brMap = ['sprint':'main','acc':'release','prod':'hotfix']
+def brs = brMap[Phase]
+String rS = "No \${brs} branch found"
+return [rS]
+""", true)
+        def dynBranch = new SecureGroovyScript("""
+def repurl = '${repoApp}'
+def brMap = ['sprint':['main'],'acc':['release*'],'prod':['hotfix*']]
+String[] brList = []
+brMap[Fase].each {
+	def proc = "git ls-remote -h \${repurl} \${it}".execute()
+	String[] out = proc.text.split('\\n')
+	brList += (out - '')
 }
+if (brList.size() < 1) throw RuntimeException("1")
+def res = brList.collect {elem -> elem.split('heads/')[1]}
+return res.reverse()
+""", false)
 
-return choices
-""",userId,fruit_fans)
+        println "Jenkins job ${env.JOB_NAME} gets updated."
+        currentBuild.displayName = "#" + Integer.toString(currentBuild.number) + ": Initialize job"
 
-properties([
-    parameters([
-        [
-            $class: 'ChoiceParameter',
-            choiceType: 'PT_MULTI_SELECT',
-            description: 'Active Choices Reactive parameter',
-            filterLength: 1,
-            filterable: false,
-            name: 'food',
-            referencedParameters: 'role',
-            script: [
-                $class: 'GroovyScript',
-                fallbackScript: [
-                    classpath: [],
-                    sandbox: false,
-                    script: 'return ["error"]'
-                ],
-                script: [classpath: [],
-                    sandbox: false,
-                    script: code
-                ]
-            ]
-
+        newParams += [$class: 'ChoiceParameterDefinition', name: 'Phase', choices: ['sprint', 'acc', 'prod']]
+        newParams += [$class: 'CascadeChoiceParameter',
+                      name: 'Branch',
+                      referencedParameters: 'Phase',
+                      script: [ $class: 'GroovyScript',
+                                script: dynBranch,
+                                fallbackScript: dynBranchFB
+                      ],
+                      choiceType: 'PT_SINGLE_SELECT',
+                      description: 'De name of the branch.'
         ]
-    ])
-])
-node {
-    stage("Show Selection"){
-        echo "$userId selected $food" 
+        newParams += [$class: 'BooleanParameterDefinition', name: 'RegenerateJob', defaultValue: false]
+
+        props += [$class: 'ParametersDefinitionProperty', parameterDefinitions: newParams]
+        properties(properties: props)
     }
 }
