d6d7049d0f5bc29edc4af59e80f17232a77460c4
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-15 16:54:12 +0300

    Update test4.groovy

--- test4.groovy
+++ test4.groovy
@@ -21,22 +21,23 @@ def getServers(){
     list.each {   
         listServer.add(it.key)
     }
-    println listServer
+    return listServer
     /*def playbookListFile = readFile("test.txt").readLines()
     playbookListFile.each {
       listServer.add(it)
     }*/
-
-        jobParameters.add([$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT',   name: 'Servers', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'], script: [classpath: [], sandbox: true,
-            script:  listServer]]])
 }
+
 def getDB(String Servers) {
     list[$Servers].each {   
         listDB.add(it)
     }
-    jobParameters.add([$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT',   name: 'Servers', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'], script: [classpath: [], sandbox: true,
-            script: listDB]]])
+    return listDB
 }
+jobParameters.add([$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT',   name: 'Servers', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'], script: [classpath: [], sandbox: true,
+            script:  getServers()]]])
+jobParameters.add([$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT',name: 'DB', referencedParameters: 'Servers', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["error"]'], script: [classpath: [], sandbox: true, 
+            script: getDB($Servers)]]])
 properties([
     parameters(jobParameters)
 ])
