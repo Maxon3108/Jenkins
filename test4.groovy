71b99174fa788504e11abd4086a6932e1ffc72db
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-15 17:15:58 +0300

    Update test4.groovy

--- test4.groovy
+++ test4.groovy
@@ -35,7 +35,8 @@ def getDB(String Servers, list) {
 if(binding.hasVariable('$Servers')) {
 listDB = getDB($Servers)
 } else {
-    listDB = getDB('Select:selected', list)
+    $Servers = listServer[0]
+    listDB = getDB($Servers)
 }
 jobParameters.add([$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT',name: 'DB', referencedParameters: 'Servers', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["error"]'], script: [classpath: [], sandbox: true, 
             script: listDB]]])
