34fa26646ce73c0feca93299e07e8c37e0458f05
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-15 15:43:27 +0300

    Update test4.groovy

--- test4.groovy
+++ test4.groovy
@@ -12,12 +12,19 @@ stages {
    }
  }
 node() {
+def list = ['Server1': ['DB1_1', 'DB1_2']
+            'Server2': ['DB2_1', 'DB2_2', 'DB2_3']
+           ]
 def jobParameters = []
 def listServer = []
-def playbookListFile = readFile("test.txt").readLines()
+def listDB = []
+/*def playbookListFile = readFile("test.txt").readLines()
 playbookListFile.each {
   listServer.add(it)
-}
+}*/
+    list.eachWithIndex {
+            listServer.add(it)    
+    }
     jobParameters.add(choice(choices: listServer, description: '', name: 'Choice'))
 properties([
     parameters(jobParameters)
