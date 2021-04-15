9b2aeb7657e09ee16c58beb003803c0d95d1eae3
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-15 15:44:10 +0300

    Update test4.groovy

--- test4.groovy
+++ test4.groovy
@@ -12,7 +12,7 @@ stages {
    }
  }
 node() {
-def list = ['Server1': ['DB1_1', 'DB1_2']
+def list = ['Server1': ['DB1_1', 'DB1_2'],
             'Server2': ['DB2_1', 'DB2_2', 'DB2_3']
            ]
 def jobParameters = []
