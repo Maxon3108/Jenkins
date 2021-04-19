ee4e23e0dded5e3a97cf13efd55f5026a2405b2b
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-19 21:55:22 +0300

    Update test55.groovy

--- test55.groovy
+++ test55.groovy
@@ -16,15 +16,28 @@ node() {
 
     }
   }
-  metaSend(fpEnable)
+  
 }
 def metaSend(List fpEnable) {
-  stage('Curl') {
+  
     sh "ls -l"
     fpEnable.each {
       sh "cat meta/${it}/PD.json"
       sh "cat meta/${it}/BS.json"
     }
 
-  }
+  
 }
+
+pipeline {
+    agent any
+stages {
+    stage('Curl'){
+    steps {
+      
+       metaSend(fpEnable)
+       
+      }
+    }
+   }
+ }
