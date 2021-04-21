6f6ba01fc8371e6f34fdd3f9c159d72e4bb0fa02
Maxon3108 <45070377+Maxon3108@users.noreply.github.com>
2021-04-21 09:29:54 +0300

    Update test4.groovy

--- test4.groovy
+++ test4.groovy
@@ -11,24 +11,25 @@ stages {
     }
    }
  }
-List list = ['Selecet:selected': 'Not Applicable', 'Server1': ['DB1_1', 'DB1_2'], 'Server2': ['DB2_1', 'DB2_2', 'DB2_3']]
+List list = ["\"Selecet:selected\"": "\"Not Applicable\"", "\"Server1\"": ["\"DB1_1\"","\"DB1_2\""], "\"Server2\"": ["\"DB2_1\"", "\"DB2_2\"", "\"DB2_3\""]]
 def jobParameters = []
 def listDB = []    
-def getServers(list) {
-    List listServer = []
+List listServer = []
     list.each {   
         listServer.add(it.key)
     }
+def getServers(List values) {
+    
     /*def playbookListFile = readFile("test.txt").readLines()
     playbookListFile.each {
       listServer.add(it)
     }*/
     
-    return "return $listServer"
+    return "return $values"
 }
-String listServers = getServers(list)
+String listS = getServers(listServer)
 jobParameters.add([$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT',   name: 'Servers', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'], script: [classpath: [], sandbox: true,
-            script:  listServers]]])
+            script:  listS]]])
 
 /*def getDB(String Servers, list) {
     list[$Servers].each {   
